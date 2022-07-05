702903407518ee92b255219250392a5ab08a4e65
WooSungShin <sws326666@gmail.com>
2022-07-05 20:30:55 +0900

    my first flutter study

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'First app',
+      theme: ThemeData(primarySwatch: Colors.yellow),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('app bar title'),
+      ),
+      body: Center(
+          child: Column(
+        children: [Text('data'), Text('HI')],
+      )),
+    );
+  }
+}
